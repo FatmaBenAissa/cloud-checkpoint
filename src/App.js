fbfbee43764b8b5b3d322d2e9985545cf0db8469
fatmaBenAissa <fatmabenaissa@gmail.com>
2023-05-15 12:20:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navbarr from "./components/Navbarr";
-import GameList from "./components/GameList";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import { Route, Routes } from "react-router-dom";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 function App() {
+  const item = {
+    name: "Location 1",
+    location: {
+      lat: 40.60785,
+      lng: 10.96899,
+    },
+  };
+
+  const mapStyles = {
+    height: "400px",
+    width: "100%",
+  };
+
+  const defaultCenter = {
+    lat: 40.60785,
+    lng: 20.96899,
+  };
+
   return (
     <div>
-      <Navbarr />
-
-      <Routes>
-        <Route path="/" element={<GameList />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-
-      
+      <div className="aboutUsElement">
+        <LoadScript googleMapsApiKey="AIzaSyC-9HBtGH2Tn5omz6Y4g9B29Jg5EVVx2xE">
+          <GoogleMap
+            mapContainerStyle={mapStyles}
+            zoom={15}
+            center={defaultCenter}
+          >
+            <Marker key={item.name} position={item.location} />
+          </GoogleMap>
+        </LoadScript>
+      </div>
     </div>
   );
 }
